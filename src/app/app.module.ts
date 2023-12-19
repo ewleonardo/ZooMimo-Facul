07f4673d1504aa158b12d15aa6db993465c28438
ejunior <ewerton.junior14@outlook.com>
2023-12-19 11:11:10 -0300

    Firebase import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { EventService } from './demo/service/event.service';
 import { IconService } from './demo/service/icon.service';
 import { NodeService } from './demo/service/node.service';
 import { PhotoService } from './demo/service/photo.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment'
 
 @NgModule({
     declarations: [
